1d0fc4fc9eae4c894acf4bd3b095ddfa5dc676e6
Eduardo Matos <eduardo-matos004@hotmail.com>
2022-11-02 21:08:26 -0300

    feat: adding prometheus metrics

--- python-api-project/app/api.py
+++ python-api-project/app/api.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
+from starlette_exporter import PrometheusMiddleware, handle_metrics
+from dynaconf import settings
 
 from app.routers import root
 from app.routers import health_check
 
 app = FastAPI(
-    title="Python API Project",
-    description="Python API Project",
+    title=settings.get("application_name"),
+    description=settings.get("application_description"),
     version="v0.1.0",
 )
+
+app.add_middleware(PrometheusMiddleware, app_name=settings.get("application_name"))
+app.add_route("/metrics", handle_metrics)
 
 app.include_router(root.router)
 app.include_router(health_check.router)
--- python-api-project/run.py
+++ python-api-project/run.py
@@ -1,7 +1,17 @@
+from logging import getLogger
+
 from uvicorn import run
 
+from app.common.log import (
+    create_logger,
+    get_application_log_level,
+    get_application_access_log,
+)
 from app.api import app as application
 
+
+create_logger()
+logger = getLogger()
 
 if __name__ == "__main__":
     run(
@@ -9,4 +19,6 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=8000,
         workers=4,
+        log_level=get_application_log_level().lower(),
+        access_log=get_application_access_log(),
     )
